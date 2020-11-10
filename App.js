86b0a28e6bfcb041b4c5c0c6783810d8d96fe5f8
unknown <rajendilani07@gmail.com>
2020-11-10 02:19:15 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React,{Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import LoginForm from './Authentication/LoginForm';
+import SignUpForm from './Authentication/SignUpForm';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import ForgetPassword from './Authentication/ForgetPassword.js'
+export default class App extends Component{
+  render(){
+    return(
+      <LoginForm/>
+    )
+  }
+}
